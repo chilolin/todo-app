08e9dae77e1135ea69da0aee80e37a0d364814e2
chilolin <67007822+chilolin@users.noreply.github.com>
2021-01-17 18:52:58 +0900

    Update firebase.sh

--- .github/scripts/firebase.sh
+++ .github/scripts/firebase.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+if [ -z $FIREBASE_TOKEN ]; then
+  echo "FIREBASE_TOKEN is required to run commands with the firebase cli"
+  exit 126
+fi
+
 if [ -n "$FIREBASE_PROJECT_ID" ]; then
   echo "Setting firebase project to $FIREBASE_PROJECT_ID"
   firebase use --add "$FIREBASE_PROJECT_ID"
 fi
 
-echo "Will apply target."
-
-firebase target:apply hosting $FIREBASE_PROJECT_ID $FIREBASE_PROJECT_ID
-
 echo "Target was set. Will deploy"
 
-firebase deploy
+firebase deploy --token=${{ $FIREBASE_TOKEN }}
