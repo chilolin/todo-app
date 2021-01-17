75ab82d1affc1a9eb760606bd841b50eda2b68f0
chilolin <67007822+chilolin@users.noreply.github.com>
2021-01-17 18:36:41 +0900

    Update firebase.sh

--- .github/scripts/firebase.sh
+++ .github/scripts/firebase.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -n "$FIREBASE_PROJECT_ID" ]; then
   echo "Setting firebase project to $FIREBASE_PROJECT_ID"
-  firebase use --add "$FIREBASE_PROJECT_ID"
+  firebase use "$FIREBASE_PROJECT_ID"
 fi
 
 echo "Will apply target."
