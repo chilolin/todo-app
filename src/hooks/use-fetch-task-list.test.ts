db6270365c933ccbdd6a0b3c581564290888da5d
chilolin <yuma321ball@icloud.com>
2021-02-06 12:10:34 +0900

    failed to write hook test

--- src/hooks/use-fetch-task-list.test.ts
+++ src/hooks/use-fetch-task-list.test.ts
@@ -1,18 +1,21 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { waitFor } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 
 import useFetchTaskList from 'hooks/use-fetch-task-list';
 import * as reducers from 'features/todo/todoSlice';
 import * as firebaseUtils from 'firebase/utils';
 
 const mockUseDispatch = jest.fn();
-const mockUseSelector = jest.fn(() => ({
-  '123': {
-    id: '123',
-    title: 'task0',
-    deadline: '2021-01-29',
-    createdAt: new Date(2021, 1, 28),
-  },
-}));
+const mockUseSelector = jest.fn(
+  (): reducers.TaskList => ({
+    '123': {
+      id: '123',
+      title: 'task0',
+      deadline: '2021-01-29',
+      createdAt: new Date(2021, 1, 28),
+    },
+  }),
+);
 
 jest.mock('react-redux', () => ({
   useDispatch: () => (): unknown => mockUseDispatch(),
@@ -37,21 +40,21 @@ describe('useFetchTaskListフックのテスト', () => {
       },
     };
 
-    // const mockFetchTaskList = jest.spyOn(reducers, 'fetchTaskList');
+    const mockFetchTaskList = jest.spyOn(reducers, 'fetchTaskList');
 
     const mockGetTaskList = jest
       .spyOn(firebaseUtils, 'getTaskList')
       .mockImplementation(() => Promise.resolve(taskList));
 
-    await act(async () => {
-      const { waitForNextUpdate, rerender } = renderHook(() =>
-        useFetchTaskList(),
-      );
+    const { result } = renderHook(() => useFetchTaskList());
 
-      rerender();
-      await waitForNextUpdate();
+    expect(result.current.isLoading).toEqual(true);
+
+    await waitFor(() => {
+      expect(mockGetTaskList).toHaveBeenCalled();
     });
 
-    expect(mockGetTaskList).toHaveBeenCalledTimes(2);
+    expect(mockFetchTaskList).toHaveBeenCalled();
+    expect(result.current.isLoading).toEqual(false);
   });
 });
